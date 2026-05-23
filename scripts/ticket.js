ecf3fd7c5849faa604ce727755b95b07b7e4e2ce
devserao <seraotourjo@gmail.com>
2026-05-23 16:18:31 +0600

    added Navbar and hero sections

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -1,1 +1,8 @@
-console.log("ticket");
+// navBar Toggle Menu
+
+const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
+console.log(navLinks);
+menuToggle.addEventListener('click', function (){
+    navLinks.classList.toggle("active");
+})
